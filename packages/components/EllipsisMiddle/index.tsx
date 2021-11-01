91da3a2947345c637713325f900c57bb50afb093
GitKou <elizagao@msn.cn>
2021-11-01 21:00:35 +0800

    add: add a component

---
+++ packages/components/EllipsisMiddle/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Typography } from 'antd';
+import type { TextProps } from 'antd/lib/typography/Text';
+
+const { Text } = Typography;
+
+export interface EllipsisMiddleProps extends TextProps {
+  suffixCount: number;
+  children?: string;
+}
+
+const EllipsisMiddle = ({ suffixCount, children, ...restProps }: EllipsisMiddleProps) => {
+  const start = children?.slice(0, children?.length - suffixCount).trim();
+  const suffix = children?.slice(-suffixCount).trim();
+  return (
+    <Text ellipsis={{ suffix }} {...restProps}>
+      {start}
+    </Text>
+  );
+};
+
+export default EllipsisMiddle;
