d242cae8f1a70783fd6a142a9d3d09cf2187ba98
GitKou <elizagao@msn.cn>
2022-01-14 13:09:40 +0800

    chore: copy less for lib

--- packages/components/gulpfile.js
+++ packages/components/gulpfile.js
@@ -1,8 +1,12 @@
 const commonConfig = require('../../gulpfile');
 const gulp = require('gulp');
 
-gulp.task('less', function () {
+gulp.task('esLess', function () {
   return gulp.src(['src/**/*.less']).pipe(gulp.dest('es/'));
 });
 
-exports.default = gulp.series(commonConfig.default, 'less');
+gulp.task('libLess', function () {
+  return gulp.src(['src/**/*.less']).pipe(gulp.dest('lib/'));
+});
+
+exports.default = gulp.series(commonConfig.default, 'esLess', 'libLess');
